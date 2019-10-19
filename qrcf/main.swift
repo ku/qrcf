fbf9599810b46770d8315fadf99253b3bc5a260e
Kentaro ku KUMAGAI <ku0522a@gmail.com>
2019-10-19 09:21:40 +0900

    Initial Commit

---
+++ qrcf/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  qrcf
+//
+//  Created by ku KUMAGAI Kentaro on 2019/10/19.
+//  Copyright © 2019 ku KUMAGAI. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
